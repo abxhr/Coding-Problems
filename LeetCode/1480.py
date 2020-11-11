19765a7d46138ea1e6a927f4e7814ad5dc1f43be
abshar <absharaslam2001@gmail.com>
2020-11-11 13:23:35 +0400

    Changes to 1480.py

--- LeetCode/1480.py
+++ LeetCode/1480.py
@@ -1,7 +1,11 @@
 string = input()
 nums = [int(i) for i in string.strip('[]').split(',')]
 s = 0
+string ="["
 for i in range(len(nums)):
     nums[i] += s
     s = nums[i]
-print(nums)
+    string += str(nums[i]) + ","
+string = string[:-1] + "]"
+print(string)
+print("blah")
