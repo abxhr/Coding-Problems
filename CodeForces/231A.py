b3e33749ce789476a38e7816e0793a01ce450c81
abshar <absharaslam2001@gmail.com>
2020-11-12 15:27:19 +0400

    Add 231A.py to repo

---
+++ CodeForces/231A.py
@@ -0,0 +1,16 @@
+n = int(input())
+c = 0
+s= []
+for i in range(n):
+	a = input()
+	s.append(a.split())
+temp=0
+for i in s:
+	for j in i:
+		if int(j) == 1:
+			temp +=1 
+	if temp > 1:
+		c += 1
+	temp=0
+
+print(c)
