5c586afadb6a26f27972003daad8ea9f810a52a8
abshar <absharaslam2001@gmail.com>
2020-11-13 23:33:21 +0400

    Add easy8.py

---
+++ HackerRank/Easy/easy8.py
@@ -0,0 +1,16 @@
+n = int(input())
+a= []
+
+for i in (input().split()):
+  a.append(int(i))
+
+lst = [float("-inf"), float("-inf")]
+for i in a:
+  if i > lst[0]:
+    lst[1] = lst[0]
+    lst[0] = i
+  elif i < lst[0]:
+    if i > lst[1]:
+      lst[1] = i
+  
+print(lst[1])
