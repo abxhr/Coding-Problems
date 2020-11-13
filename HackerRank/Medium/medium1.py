bbd69e0870299cc27d9f37ca6dd99ad1fd3f226a
abshar <absharaslam2001@gmail.com>
2020-11-13 23:28:38 +0400

    Add Medium repo to HackerRank

---
+++ HackerRank/Medium/medium1.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0:
+        if year % 100 == 0:
+            leap = False
+            if year % 400 == 0:
+                leap = True
+        else:
+            leap = True
+    return leap
+
+year = int(input())
