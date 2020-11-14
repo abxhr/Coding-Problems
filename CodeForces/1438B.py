c3eb9647154b88bc55fea85addddc6e442803c4d
abshar <absharaslam2001@gmail.com>
2020-11-14 21:54:32 +0400

    Add 1438B.py (contest)

---
+++ CodeForces/1438B.py
@@ -0,0 +1,17 @@
+t = int(input())
+for i in range(t):
+  n = int(input())
+  lst = list(map(int, input().split()))
+  lst.sort()
+  for i in range(len(lst)):
+    nlst = lst[:i]
+    f = 0
+    for j in nlst:
+      if j == lst[i]:
+        print("YES")
+        f = 1
+        break
+    if f:
+      break
+  if f != 1:
+    print("NO")
