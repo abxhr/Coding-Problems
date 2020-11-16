107d1c605b917f75d31fe889242e4e18025f4c91
abshar <absharaslam2001@gmail.com>
2020-11-16 16:05:14 +0400

    Add 1342.py

---
+++ LeetCode/1342.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        c = 0
+        while num!= 0:
+            if num % 2 == 0:
+                num /= 2
+                c += 1
+            else:
+                num -= 1
+                c += 1
+        return(c)
