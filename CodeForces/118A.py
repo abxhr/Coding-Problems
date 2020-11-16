a2292295fe0e532fb3ef598f62ee6552468c9ea5
abshar <absharaslam2001@gmail.com>
2020-11-16 14:23:04 +0400

    Add 118A.py

---
+++ CodeForces/118A.py
@@ -0,0 +1,11 @@
+s = list(input())
+vowels = ['a', 'e', 'i', 'y', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'Y']
+str = ""
+for letter in s:
+    if letter not in vowels:
+        str += '.'
+        if letter.isupper():
+            str += letter.lower()
+        else:
+            str += letter
+print(str)
