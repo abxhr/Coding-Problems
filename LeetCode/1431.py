8150eca9624d08826283fc4804764fe08a29d6d5
abshar <absharaslam2001@gmail.com>
2020-11-17 07:34:34 +0400

    Add 1431.py

---
+++ LeetCode/1431.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        blst = []
+        greatest = candies[0]
+        for i in candies:
+            if i > greatest:
+                greatest = i
+        for i in candies:
+            if i + extraCandies >= greatest:
+                blst.append(True)
+            else:
+                blst.append(False)
+        return blst
