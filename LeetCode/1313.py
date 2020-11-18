cbd2f22e5a6a1624d09a5e44c836404f33425f84
abshar <absharaslam2001@gmail.com>
2020-11-18 07:36:52 +0400

    Add 1313.py

---
+++ LeetCode/1313.py
@@ -0,0 +1,9 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        lst = []
+        for i in range(int(len(nums)/2)):
+            freq = nums[2*i]
+            val = nums[2*i+1]
+            for i in range(freq):
+                lst.append(val)
+        return(lst)
