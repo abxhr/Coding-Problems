1f3711126c10e66a0450bea34780770b2be2983c
abshar <absharaslam2001@gmail.com>
2020-11-20 02:11:16 +0400

    Add 405A.cpp

---
+++ CodeForces/405A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+    }
+    int temp;
+    for(int i = 0; i<n; i++) {
+        for(int j = i+1; j<n; j++){
+            if(a[j] < a[i]) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            }
+        }
+    }   
+    for(int i = 0; i < n; ++i){
+        cout << a[i] << " ";
+    }
+    }
+    
