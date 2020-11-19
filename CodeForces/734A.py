539f6c1cc5a339d953be5f74351f97e4ebbf04bc
abshar <absharaslam2001@gmail.com>
2020-11-20 00:53:04 +0400

    Add 734A.py

---
+++ CodeForces/734A.py
@@ -0,0 +1,15 @@
+n = int(input())
+d = 0
+a = 0
+s = input()
+for i in list(s):
+    if i == "A":
+        a += 1
+    else:
+        d += 1
+if a > d:
+    print("Anton")
+elif d > a:
+    print("Danik")
+else: 
+    print("Friendship")
