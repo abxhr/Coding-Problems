0dda58a36625d9b7471acbd3820a7740a03eaac8
abshar <absharaslam2001@gmail.com>
2020-11-21 10:00:57 +0400

    Add 771.py

---
+++ LeetCode/771.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        j = list(J)
+        s = list(S)
+        c = 0
+        for i in s:
+            if i in j:
+                c += 1
+        return(c)
