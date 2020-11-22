45e3ba9d9e065319d46620401397896e14f64c12
abshar <absharaslam2001@gmail.com>
2020-11-22 07:59:20 +0400

    Add 427A.cpp

---
+++ CodeForces/427A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    int h = 0;
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++){
+        if(a[i] > 0) h += a[i];
+        else{
+            if(h){
+                h -= 1;
+            }
+            else count += 1;
+        }
+    }
+    cout << count;
+}
