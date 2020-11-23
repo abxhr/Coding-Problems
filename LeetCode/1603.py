86867d76443a10adf5e144863e9f3846840d76a7
abshar <absharaslam2001@gmail.com>
2020-11-23 08:05:30 +0400

    Add 1603.py

---
+++ LeetCode/1603.py
@@ -0,0 +1,27 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small
+
+    def addCar(self, carType: int) -> bool:
+        f = True
+        if carType == 1:
+            if self.big > 0:
+                self.big -= 1
+                return True
+            else:
+                f = False
+        elif carType == 2:
+            if self.medium > 0:
+                self.medium -= 1
+                return True
+            else:
+                f = False
+        elif carType == 3:
+            if self.small > 0:
+                self.small -= 1
+                return True
+            else:
+                return False
