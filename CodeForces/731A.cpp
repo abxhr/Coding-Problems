f49e7476365775c80d4c0e4f2e2646695aa76fd0
abshar <absharaslam2001@gmail.com>
2020-12-04 17:11:01 +0400

    Add 731A.cpp

---
+++ CodeForces/731A.cpp
@@ -0,0 +1,26 @@
+//a = 97
+//total = 26
+//half = 13
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string in;
+    int count = 0;
+    cin >> in;
+    int flag, diff;
+    flag = 97;
+    for(int i=0;i<in.length();i++){
+        diff = abs(flag - in[i]);
+        if(diff > 13){
+            diff = 26 - diff;
+        }
+        count += diff;
+        flag = in[i];
+    }
+    cout << count;
+    return 0;
+}
