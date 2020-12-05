b7dd42e5e45b65557f3112f8d81d8482f0d4299a
abshar <absharaslam2001@gmail.com>
2020-12-05 22:16:26 +0400

    Add 268A.cpp

---
+++ CodeForces/268A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n],b[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i] >> b[i];
+    }
+    int count = 0;
+    for(int i = 0;i<n;i++){
+        for(int j =0; j<n;j++){
+            if(i!=j){
+                if(a[i] == b[j]) count++;
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
