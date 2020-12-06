a16da6c5fd26f964b3f197fa92e15c05241ff090
abshar <absharaslam2001@gmail.com>
2020-12-06 14:18:26 +0400

    Add 732A.cpp

---
+++ CodeForces/732A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int k,r;
+    cin >> k >> r;
+    bool f = true;
+    for(int i = 1;f;i++){
+        if((k*i) == (((k*i)/10)*10)){
+            cout << i;
+            f = false;
+        }
+        else if ((k*i) == ((((k*i)/10)*10) + r)){
+            cout << i;
+            f = false;
+        }
+    }
+    return 0;
+}
