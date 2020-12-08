afa2bf9bda055ebd11658dd5d7095878c4afe278
abshar <absharaslam2001@gmail.com>
2020-12-08 11:37:29 +0400

    Add 799A.cpp:

---
+++ CodeForces/799A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, t, k, d;
+    cin >> n >> t >> k >> d;
+    
+    if(d < t*((ceil(n*1.0/k)) - 1)){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+
+    return 0;
+}
