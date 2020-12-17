03a4d6d429251703a64d736959c833c48b351bb9
abshar <absharaslam2001@gmail.com>
2020-12-17 21:48:46 +0400

    Add 766 A.cpp

---
+++ CodeForces/766A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+    if(a==b){
+        cout << "-1";
+    }
+    else{
+        cout << max(a.length(),b.length()); 
+    }
+    return 0;
+}
