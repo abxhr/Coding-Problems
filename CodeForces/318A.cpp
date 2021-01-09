ca5e95ffdde19c43525f3f6f54816c8798ffd746
abshar <absharaslam2001@gmail.com>
2021-01-09 08:23:37 +0400

    [CF] Add 318A.cpp

---
+++ CodeForces/318A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long k, val;
+    long double n;
+    cin >> n >> k;
+    long long c = ceil(n/2);
+    if(k <= c){
+        val = 2*k - 1;
+    }
+    else{
+        val = 2*(k - c);
+    } 
+    cout << val;
+    return 0;
+}
