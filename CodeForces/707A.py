6b75fde81f3948e9bdb7356c19721842507dbac7
abshar <absharaslam2001@gmail.com>
2021-01-11 07:19:42 +0400

    [CF] Add 707A.py

---
+++ CodeForces/707A.py
@@ -0,0 +1,13 @@
+import sys
+
+n, m = map(int,input().split())
+c = []
+bw = ['W', 'B', 'G']
+for i in range(n):
+    c.append(input().split())
+for i in c:
+    for j in i:
+        if j not in bw:
+            print("#Color")
+            sys.exit(0)
+print("#Black&White")
