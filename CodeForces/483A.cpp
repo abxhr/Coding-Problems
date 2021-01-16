2229c79b35b6a5fd813798c4099992784cc63cf0
abshar <absharaslam2001@gmail.com>
2021-01-16 20:59:20 +0400

    [CF] Add 483A.cpp

---
+++ CodeForces/483A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	long long int l,r;
+	cin >> l >> r;
+	if (l%2 != 0) l++;
+	if (l+2>r) cout << -1;
+	else cout << l << " " << l+1 << " " << l+2;
+	return 0;
+}
