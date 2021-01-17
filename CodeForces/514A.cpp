7d2e8fb3cfc9bc6e1d8373496640de64a55a1b8e
abshar <absharaslam2001@gmail.com>
2021-01-17 08:36:19 +0400

    [CF] Add 514A.cpp

---
+++ CodeForces/514A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	long long x;
+	cin >> x;
+	int digit;
+	int arr[20];
+	int i=0;
+	while(x>0){
+		digit = x%10;
+		if(9-digit<digit) arr[i] = 9-digit;
+		else arr[i] = digit;
+		i++;
+		x = x/10;
+	}
+	if(arr[i-1]==0){
+		cout << 9;
+		i--;
+	}
+	for(int j=i-1;j>=0;--j){
+		cout << arr[j];
+	}
+	return 0;
+}
