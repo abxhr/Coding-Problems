2db114b48e8a39ed75b75fb56329c4641ce75e23
abshar <absharaslam2001@gmail.com>
2021-01-18 20:13:27 +0400

    [CF] Add 1204A.cpp

---
+++ CodeForces/1204A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+	long long int p=s.length();
+	if(p%2==0) cout<<(p/2);
+	else{
+		int count1=0;
+		for(int i=0;i<s.length();i++){
+			if(s[i]=='1') count1++;
+		}
+		if(count1>1){
+			int ans=p/2;
+			cout<<(ans+1);
+		}
+		else{
+			int ans=p/2;
+			cout<<ans;
+		}
+	}
+	return 0;
+}
+
