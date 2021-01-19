f58da2ef23d557be395c2441b7fb833b8130b33e
Abshar Mohammed Aslam <Absharaslam2001@gmail.com>
2021-01-19 09:37:49 +0400

    [LC] Add 1720.py

---
+++ LeetCode/1720.py
@@ -0,0 +1,8 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr = [first]
+        for i in range(0,len(encoded)):
+            arr.append(arr[i]^encoded[i])
+        return arr
