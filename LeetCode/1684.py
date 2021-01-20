b26dc58ccbc7b3101e29dd8e250bd6fc6c0652fc
abshar <absharaslam2001@gmail.com>
2021-01-20 13:05:27 +0400

    [LC] Add 1684.py

---
+++ LeetCode/1684.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        n = 0
+        for i in words:
+            f = True
+            for j in i:
+                if j not in allowed:
+                    f = False
+                    break
+            if f:
+                n += 1
+        return n
