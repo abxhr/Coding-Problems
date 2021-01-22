2e1fbe8247f016fabe958a82658c24a4c0bda77e
abshar <absharaslam2001@gmail.com>
2021-01-22 14:54:15 +0400

    [LC] Add 804.py

---
+++ LeetCode/804.py
@@ -0,0 +1,13 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        alpha = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        mlist = []
+        for word in words:
+            morse = ''
+            for l in word:
+                val = ord(l)-97
+                morse += alpha[val]
+            mlist.append(morse)
+        return(len(set(mlist)))
