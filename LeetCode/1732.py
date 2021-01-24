66371aa6b93b3c6951f8fb23b59b5f6cc6f19208
abshar <absharaslam2001@gmail.com>
2021-01-24 12:07:36 +0400

    [LC] Add 1732.py

---
+++ LeetCode/1732.py
@@ -0,0 +1,11 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        max = 0 
+        val = 0
+        for i in gain:
+            val += i
+            if val > max:
+                max = val
+        return max
