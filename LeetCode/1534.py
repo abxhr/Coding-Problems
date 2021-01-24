22464c65ed045bc1815ac5021d0692233f065b96
abshar <absharaslam2001@gmail.com>
2021-01-24 12:19:47 +0400

    [LC] Add 1534.py

---
+++ LeetCode/1534.py
@@ -0,0 +1,12 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        n = 0
+        for i in range(0,len(arr)-2):
+            for j in range(1,len(arr)-1):
+                for k in range(2,len(arr)):
+                    if i < j < k:
+                        if (abs(arr[i]-arr[j])<=a) and (abs(arr[j]-arr[k])<=b) and (abs(arr[i]-arr[k])<=c):
+                            n += 1
+        return n
