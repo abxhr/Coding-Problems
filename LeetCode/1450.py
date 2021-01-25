3c8f372074e2834cdf9ce10c9eb96ffb7638bb07
abshar <absharaslam2001@gmail.com>
2021-01-25 07:16:28 +0400

    [LC] Add 1450.py

---
+++ LeetCode/1450.py
@@ -0,0 +1,9 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        c = 0
+        for i in range(len(startTime)):
+            if (startTime[i]<=queryTime) and (endTime[i]>=queryTime):
+                c += 1
+        return c
