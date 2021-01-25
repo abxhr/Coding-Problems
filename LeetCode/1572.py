220c6fb9c611142ac343fdb6ec6a45b224b41300
abshar <absharaslam2001@gmail.com>
2021-01-25 06:46:37 +0400

    [LC] Add 1572.py

---
+++ LeetCode/1572.py
@@ -0,0 +1,13 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        sum = 0
+        v = int(len(mat))-1
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+                if i==j:
+                    sum += mat[i][j]
+                elif i+j == v:
+                    sum += mat[i][j]
+        return sum
