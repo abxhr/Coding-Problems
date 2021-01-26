617718e758360c4865d766496e2b11d167553350
abshar <absharaslam2001@gmail.com>
2021-01-26 16:15:55 +0400

    [LC] Add 1207.py

---
+++ LeetCode/1207.py
@@ -0,0 +1,20 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        lsto = []
+        l = len(arr)
+        for i in set(arr):
+            c = 1
+            for j in range(l):
+                if i==arr[j] and arr[j]!=1001:
+                    c += 1
+                    arr[j] = 1001
+            lsto.append(c)
+        lsto.sort()
+        chk = list(set(lsto))
+        chk.sort()
+        if lsto == chk:
+            return True
+        else:
+            return False
