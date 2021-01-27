694a231862af8da4ff21c53207500a7efe6a20d8
abshar <absharaslam2001@gmail.com>
2021-01-27 08:37:05 +0400

    [LC] Add 1051.py

---
+++ LeetCode/1051.py
@@ -0,0 +1,10 @@
+# Author: Abshar Mohammed Aslam, gitub.com/abxhr
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        s = sorted(heights)
+        c = 0
+        for i in range(len(heights)):
+            if s[i]!=heights[i]:
+                c+=1
+        return c
