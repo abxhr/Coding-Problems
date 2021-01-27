c59e8f7b6fc326c8fcdd578dfb469904168c8205
abshar <absharaslam2001@gmail.com>
2021-01-27 07:46:38 +0400

    [LC] Add 1002.py

---
+++ LeetCode/1002.py
@@ -0,0 +1,14 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        lsta = list(A[0])
+        lstb = list(A[0])
+        for i in range(1,len(A)):
+            lsta = lstb
+            lstb = []
+            for j in A[i]:
+                if j in lsta:
+                    lstb.append(j)
+                    lsta.remove(j)
+        return lstb
