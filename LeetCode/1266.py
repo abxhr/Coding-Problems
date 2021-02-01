ee9b0fe0be8c06d6e07694a5cada25bcf618ade1
abshar <absharaslam2001@gmail.com>
2021-02-01 07:13:40 +0400

    [LC] Add 1266.py

---
+++ LeetCode/1266.py
@@ -0,0 +1,10 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        time = 0
+        prev = points[0]
+        for i in range(1,len(points)):
+            time += max(abs(points[i][0]-prev[0]),abs(points[i][1]-prev[1]))
+            prev = points[i]
+        return time
