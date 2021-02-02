7eca7c98ffa09d839d026c4e30cc760fcfec85a8
abshar <absharaslam2001@gmail.com>
2021-02-02 07:05:41 +0400

    [LC] Add 1323.py

---
+++ LeetCode/1323.py
@@ -0,0 +1,13 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num = str(num)
+        if '6' not in num:
+            return int(num)
+        num = list(num)
+        for i in range(len(num)):
+            if num[i] == '6':
+                num[i] = '9'
+                break
+        return int(''.join(num))
