405f85dcc1073416a24f80e4c422e7040a720f7b
abshar <absharaslam2001@gmail.com>
2021-02-06 20:06:39 +0400

    [LC] Add 1304.py

---
+++ LeetCode/1304.py
@@ -0,0 +1,11 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        lst = []
+        for i in range(1,int(n/2)+1):
+            lst.append(i)
+            lst.append(-i)
+        if (n%2):
+            lst.append(0)
+        return lst
