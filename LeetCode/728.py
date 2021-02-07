2225d8eaa3c90d9c595219661dda7ff2a48762af
abshar <absharaslam2001@gmail.com>
2021-02-07 07:22:04 +0400

    [LC] Add 728.py

---
+++ LeetCode/728.py
@@ -0,0 +1,17 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        lst = []
+        for i in range(left,right+1):
+            f = True
+            for j in str(i):
+                if j == '0':
+                    f = False
+                    break
+                elif i%int(j) != 0:
+                    f = False
+            if f:
+                lst.append(i)
+        return lst
+                
