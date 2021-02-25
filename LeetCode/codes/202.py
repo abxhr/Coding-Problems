1175f0154be3464417d1801c97ac405bfb34a6bb
abshar <absharaslam2001@gmail.com>
2021-02-25 11:45:20 +0400

    [LC] Add Solution for Happy Number

---
+++ LeetCode/codes/202.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def square(n):
+            sq = 0
+            while n > 0:
+                digit = n%10
+                sq += digit ** 2
+                n = n//10
+            return sq
+        used = [n]
+        val = n
+        while val != 1:
+            val = square(val)
+            if val in used:
+                return False
+            used.append(val)
+        return True
