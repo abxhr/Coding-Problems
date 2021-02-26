ef08fc24c015ee973aa73b19b89c14d485835385
abshar <absharaslam2001@gmail.com>
2021-02-26 16:01:33 +0400

    [LC] Add Solution for Distribute Candies

---
+++ LeetCode/codes/575.py
@@ -0,0 +1,5 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(set(candyType)), len(candyType)//2)
