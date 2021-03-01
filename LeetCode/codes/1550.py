240abfbc69a2470c99134e7a75051c62be9bd6f0
abshar <absharaslam2001@gmail.com>
2021-03-01 07:28:54 +0400

    [LC] Add Solution for Three Consecutive Odds

---
+++ LeetCode/codes/1550.py
@@ -0,0 +1,5 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        return '111' in ''.join(str(i%2) for i in arr)
