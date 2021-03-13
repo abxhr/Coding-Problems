1e9de60b787ad12a78adc936b9ded1c26107678b
Abshar Mohammed Aslam <absharaslam2001@gmail.com>
2021-03-13 06:42:38 +0400

    [LC] Add Solution for Replace Elements with Greatest Element on Right Side

---
+++ LeetCode/codes/1299.py
@@ -0,0 +1,16 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        if len(arr) == 1:
+            return [-1]
+        max = arr[-1]
+        for i in range(len(arr) - 1, -1, -1):
+            if arr[i] > max:
+                prev = max
+                max = arr[i]
+                arr[i] = prev
+            else:  
+                arr[i] = max
+        arr[-1] = -1
+        return arr
