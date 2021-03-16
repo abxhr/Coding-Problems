b256b2720b5b77d995f32ac3032b823d5134473b
Abshar <absharaslam2001@gmail.com>
2021-03-16 20:26:49 +0400

    [LC] Add Solution for Detect Capital

---
+++ LeetCode/codes/520.py
@@ -0,0 +1,5 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return (word.isupper() or word.islower() or word.istitle())
