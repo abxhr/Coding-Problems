05e07e26045f1f2855b94750fe7fd8f06e3a6800
Abshar <absharaslam2001@gmail.com>
2021-03-20 07:12:15 +0400

    [LC] Add Solution for Reverse String II

---
+++ LeetCode/codes/541.py
@@ -0,0 +1,15 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        ans = ""
+        i = 0
+        while (i < len(s)):
+            if i%(2*k) == 0:
+                sub_str = s[i:i+k]
+                ans += sub_str[::-1]
+                i += k
+            else:
+                ans += s[i]
+                i += 1    
+        return ans
