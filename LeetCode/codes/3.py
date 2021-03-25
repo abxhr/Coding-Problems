36926336dca6e9465e2e853c4f0354bc811e4dbc
Abshar <absharaslam2001@gmail.com>
2021-03-25 21:18:32 +0400

    [LC] Add Solution for Longest Substring Without Repeating Characters

---
+++ LeetCode/codes/3.py
@@ -0,0 +1,13 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = ''
+        lnt = 0
+        for c in s:
+            if c in chars:                
+                lnt = max(len(chars), lnt)
+                chars = chars[chars.index(c)+1:]
+            chars += c
+        lnt = max(len(chars), lnt)   
+        return lnt
