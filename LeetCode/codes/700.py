065d97bc52671fe1a33d505ae1eaa33a2758abb7
Abshar <absharaslam2001@gmail.com>
2021-03-30 13:23:27 +0400

    [LC] Add Solution for Search in a Binary Search Tree

---
+++ LeetCode/codes/700.py
@@ -0,0 +1,19 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root == None:
+            return root
+        if root.val == val:
+            return root
+        elif val > root.val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
