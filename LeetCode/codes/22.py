852935fc71a8f491ee9dbca3bb83e2676142c6d3
Abshar <absharaslam2001@gmail.com>
2021-04-04 08:18:10 +0400

    [LC] Add Solution for Generate Parentheses

---
+++ LeetCode/codes/22.py
@@ -0,0 +1,15 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def generateParenthesis(self, n):
+        def gen(left, right, ans):
+            if not 0 <= left <= right: 
+                return
+            if not right:
+                result.append(ans)
+                return
+            gen(left - 1, right, ans + '(')
+            gen(left, right - 1, ans + ')')
+        result = []
+        gen(n, n, '')
+        return result
