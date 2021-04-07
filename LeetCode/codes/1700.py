b974550a48644f859e48900472a833c89b9be483
Abshar <absharaslam2001@gmail.com>
2021-04-07 16:50:17 +0400

    [LC] Add Solution for Number of Students Unable to Eat Lunch

---
+++ LeetCode/codes/1700.py
@@ -0,0 +1,18 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        length = len(students)    
+        cycle = 0	
+        while length != cycle:
+            if students[0] == sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
+                length = len(students)
+                cycle = 0
+            else:
+                back = students[0]
+                students.pop(0)
+                students.append(back)
+                cycle += 1
+        return cycle
