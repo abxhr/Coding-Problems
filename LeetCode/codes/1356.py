4c2736a99ed9c36f30b8a0fd5dfb8338b951d95f
Abshar <absharaslam2001@gmail.com>
2021-04-13 12:24:15 +0400

    [LC] Add Solution for Sort Integers by The Number of 1 Bits

---
+++ LeetCode/codes/1356.py
@@ -0,0 +1,9 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        binary_counts = []
+        arr.sort()
+        for val in arr:
+            binary_counts.append(str(bin(val)).count('1'))
+        return [y for x,y in sorted(zip(binary_counts, arr))]
