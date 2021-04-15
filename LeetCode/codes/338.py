f182b1227a299f5293acc4378c9b2221da5a8858
Abshar <absharaslam2001@gmail.com>
2021-04-15 08:58:24 +0400

    [LC] Add Solution for Counting Bits

---
+++ LeetCode/codes/338.py
@@ -0,0 +1,8 @@
+# Author: Abshar Mohammed Aslam, github.com/abxhr
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        bits = [0]
+        for i in range(1, num+1):
+            bits.append(bits[i//2] + i%2)
+        return bits
