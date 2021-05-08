1865cab4713deff38a84fcfabc6b48cee665dc16
Abshar Mohammed Aslam <Absharaslam2001@gmail.com>
2021-05-08 23:58:25 +0400

    Create 617.py

---
+++ LeetCode/codes/617.py
@@ -0,0 +1,13 @@
+if root1 is None and root2 is None:
+            return None
+        
+        if root1 is None:
+            res = root2
+        elif root2 is None:
+            res = root1
+        else:
+            res = TreeNode(root1.val + root2.val)
+            res.left = self.mergeTrees(root1.left, root2.left)
+            res.right = self.mergeTrees(root1.right, root2.right)
+            
+        return res
